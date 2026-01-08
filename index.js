fcdaa33a51bfef87f71f8111f3ab5cecb9d5727e
Ronen-Vovsi <ronen.vovsi@gmail.com>
2026-01-07 22:54:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+$("#features .col").addClass("shadow");
+
+// Projects
+// Changing background image on hover
+$(".card").each(function(){
+  
+})
+
+// Project Redirects
+$(".card:contains('ToDo List')" ).on("click", function() {
+    window.location.href = "http://localhost:8000/login";
+})
+$(".card:contains('TinDog Website')").on("click", function () {
+  window.location.href = "./11.3+TinDog+Project/11.3 TinDog Project/index.html";
+});
+// -------------------------------
